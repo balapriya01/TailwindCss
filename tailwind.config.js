b4159e8734b135ca2d21a2c47089d6dce784a52e
Baswal <balapriya1801@gmail.com>
2024-06-21 19:22:39 +0530

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme:{
+        extend: {
+            colors:{
+                'rapunzel': '#402E7A',
+                'flower': {
+                    100: '#fef9c3',
+                    200: '#fef08a',
+                    300: '#fde047',
+                    400: '#facc15',
+                    500: '#eab308', 
+                    600: '#ca8a04',
+                    700: '#a16207',
+                    800: '#854d0e',
+                    900: '#713f12'
+
+                }
+            }
+        }
+    }
+}
